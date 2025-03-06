1353c18a5a73c4c82700d4e459a71ff35b62bfe3
minthukhant78 <minthukhant2001@outlook.com>
2025-03-06 21:32:16 +0630

    Add Supabase configuration, update HomeScreen to handle posts with type safety, and enhance .gitignore and VSCode settings

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,23 +1,36 @@
 import { View, FlatList, ActivityIndicator, Text } from "react-native";
 import PostListItem from "../../../components/PostListItem ";
-import posts from "../../../../assets/data/posts.json";
 import { useEffect, useState } from "react";
 import { supabase } from "../../../lib/supbase";
+import { Tables } from "../../../types/database.types";
+
+type Post = Tables<"posts"> & {
+  user: Tables<"users">;
+  group: Tables<"groups">;
+  };
 export default function HomeScreen() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
   useEffect(() => {
     fetchPosts();
   }, []);
 
   const fetchPosts = async () => {
-    const {data, error} = await supabase.from("posts").select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
-    setPosts(data);
+    const { data, error } = await supabase
+      .from("posts")
+      .select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
+    if (error) {
+      console.log(error);
+    } else {
+      setPosts(data);
+    }
   };
+
+
   return (
     <View>
-    <FlatList
+      <FlatList
         data={posts}
-        renderItem={({ item }) => <PostListItem post={item} />}   
+        renderItem={({ item }) => <PostListItem post={item} />}
       />
     </View>
   );

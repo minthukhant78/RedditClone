92cc7e700aec8d7c7570fcfe5a34e5f0dd139c95
minthukhant78 <minthukhant2001@outlook.com>
2025-03-06 16:00:58 +0630

    Fetch posts from Supabase and update HomeScreen component

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,7 +1,18 @@
 import { View, FlatList, ActivityIndicator, Text } from "react-native";
 import PostListItem from "../../../components/PostListItem ";
 import posts from "../../../../assets/data/posts.json";
+import { useEffect, useState } from "react";
+import { supabase } from "../../../lib/supbase";
 export default function HomeScreen() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const {data, error} = await supabase.from("posts").select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
+    setPosts(data);
+  };
   return (
     <View>
     <FlatList

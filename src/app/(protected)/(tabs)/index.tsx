57f5bd0c6b55eb42b2bb5a157d2c54007e56ff93
minthukhant78 <minthukhant2001@outlook.com>
2025-03-09 18:19:30 +0630

    Add React Query integration for posts fetching and update layout with QueryClientProvider

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -3,27 +3,39 @@ import PostListItem from "../../../components/PostListItem ";
 import { useEffect, useState } from "react";
 import { supabase } from "../../../lib/supbase";
 import { Tables } from "../../../types/database.types";
+import { useQuery } from "@tanstack/react-query";
 
 type Post = Tables<"posts"> & {
   user: Tables<"users">;
   group: Tables<"groups">;
   };
-export default function HomeScreen() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+
 
   const fetchPosts = async () => {
     const { data, error } = await supabase
       .from("posts")
       .select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
     if (error) {
-      console.log(error);
+      throw error;
     } else {
-      setPosts(data);
+      return data;
     }
   };
+export default function HomeScreen() {
+
+  const { data : posts, isLoading,error} = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPosts(),
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    return <Text>{error.message}</Text>;
+  }
+
+  
 
 
   return (

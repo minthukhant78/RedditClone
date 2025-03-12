5d2e6e69476727898dee1d8fed4552691dd70a6c
minthukhant78 <minthukhant2001@outlook.com>
2025-03-12 06:56:43 +0630

    Integrate React Query for group fetching in GroupSelector component and implement search functionality

--- src/app/(protected)/GroupSelector.tsx
+++ src/app/(protected)/GroupSelector.tsx
@@ -1,36 +1,85 @@
-import React, { useState } from 'react';
-import { FlatList, Text, TextInput, View, Image, KeyboardAvoidingView, Platform, Pressable } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
-import AntDesign from '@expo/vector-icons/AntDesign';
+import React, { useState } from "react";
+import {
+  FlatList,
+  Text,
+  TextInput,
+  View,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { Link, router } from "expo-router";
-import groups from '../../../assets/data/groups.json';
-import { useSetAtom } from 'jotai';
-import { selectedGroupAtom } from '../../atoms'
-import { Group } from '../../types';
+import { useSetAtom } from "jotai";
+import { selectedGroupAtom } from "../../atoms";
+import { Group } from "../../types";
+import { useQuery } from "@tanstack/react-query";
+import { fetchGroups } from "../../services/groupServices";
 
 export default function GroupSelector() {
-  const [searchText, setSearchText] = useState<string>('');
-  const setGroup = useSetAtom(selectedGroupAtom)
+  const [searchText, setSearchText] = useState<string>("");
+  const setGroup = useSetAtom(selectedGroupAtom);
 
-  const filteredGroups = groups.filter(group =>
-    group.name.toLowerCase().includes(searchText.toLowerCase())
-  );
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["groups", { searchText }],
+    queryFn: () => fetchGroups(searchText),
+    staleTime: 10_000,
+    placeholderData: (previousData) => previousData,
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+  if (error || !data) {
+    return <Text>Something went wrong</Text>;
+  }
 
   const onGroupSelected = (group: Group) => {
     setGroup(group);
-    router.back()
-  }
+    router.back();
+  };
 
   return (
-    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={{ flex: 1, backgroundColor: 'white', paddingHorizontal: 10 }}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={{ flex: 1, backgroundColor: "white", paddingHorizontal: 10 }}
+    >
       <SafeAreaView style={{ flex: 1 }}>
         {/* HEADER */}
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <AntDesign name="close" size={30} color="black" onPress={() => router.back()} />
-          <Text style={{ flex: 1, textAlign: 'center', paddingRight: 30, fontWeight: '600', fontSize: 15 }}>Post to</Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <AntDesign
+            name="close"
+            size={30}
+            color="black"
+            onPress={() => router.back()}
+          />
+          <Text
+            style={{
+              flex: 1,
+              textAlign: "center",
+              paddingRight: 30,
+              fontWeight: "600",
+              fontSize: 15,
+            }}
+          >
+            Post to
+          </Text>
         </View>
         {/* SEARCH BAR */}
-        <View style={{ flexDirection: 'row', backgroundColor: 'lightgrey', borderRadius: 10, gap: 5, marginVertical: 15, alignItems: 'center', paddingHorizontal: 10 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: "lightgrey",
+            borderRadius: 10,
+            gap: 5,
+            marginVertical: 15,
+            alignItems: "center",
+            paddingHorizontal: 10,
+          }}
+        >
           <AntDesign name="search1" size={16} color="black" />
           <TextInput
             placeholder="Search for a community"
@@ -40,20 +89,40 @@ export default function GroupSelector() {
             onChangeText={(text) => setSearchText(text)}
           />
           {searchText && (
-            <AntDesign name="closecircle" size={15} color="#E4E4E4" onPress={() => setSearchText('')} />
+            <AntDesign
+              name="closecircle"
+              size={15}
+              color="#E4E4E4"
+              onPress={() => setSearchText("")}
+            />
           )}
         </View>
         {/* LIST OF GROUPS */}
-        <FlatList style={{ marginTop: 10 }} data={filteredGroups} renderItem={({ item }) => (
-          <Pressable style={{ flexDirection: 'row', alignItems: 'center', gap: 5, marginBottom: 20 }} onPress={() => onGroupSelected(item)}>
-            <Image source={{ uri: item.image }} style={{ width: 40, aspectRatio: 1, borderRadius: 20 }} />
-            <View>
-              <Text style={{ fontWeight: '600' }}>{item.name}</Text>
-              <Text style={{ color: 'grey' }}>recently visited</Text>
-            </View>
-          </Pressable>
-        )} />
+        <FlatList
+          style={{ marginTop: 10 }}
+          data={data}
+          renderItem={({ item }) => (
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+                marginBottom: 20,
+              }}
+              onPress={() => onGroupSelected(item)}
+            >
+              <Image
+                source={{ uri: item.image }}
+                style={{ width: 40, aspectRatio: 1, borderRadius: 20 }}
+              />
+              <View>
+                <Text style={{ fontWeight: "600" }}>{item.name}</Text>
+                <Text style={{ color: "grey" }}>recently visited</Text>
+              </View>
+            </Pressable>
+          )}
+        />
       </SafeAreaView>
     </KeyboardAvoidingView>
-  )
+  );
 }
